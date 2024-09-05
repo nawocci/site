14864573a0c078ec19ec00a00bfcb21110532197
Naufal Altaf <me@altaf.xyz>
2024-09-05 14:01:30 +0700

    Define a proper type for mainImage and post body

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,10 +1,17 @@
+import { PortableTextBlock } from '@portabletext/types';
+
 export interface Posts {
   title: string;
   slug: { current: string };
-  mainImage: any;
+  mainImage: {
+    asset: {
+      _ref: string;
+      _type: 'reference';
+    };
+  };
   postDate: string;
 }
 
 export interface Post extends Posts {
-  body: any;
+  body: PortableTextBlock[];
 }
