82d3cfbde826f988e5acd80ebe0d36e95ab16369
Naufal Altaf <me@altaf.xyz>
2024-09-05 21:15:30 +0700

    Update mobile blog post layout and prevent overflow

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -16,20 +16,22 @@ export default async function BlogPost({ params }: { params: { slug: string } })
   }
 
   return (
-    <div className="max-w-4xl flex flex-col space-y-12 justify-center">
-      <div className="flex flex-col items-center space-y-2">
-        <p className="text-base text-secondary font-medium">{post.postDate}</p>
-        <h1 className="text-3xl sm:text-5xl font-bold text-center">{post.title}</h1>
-      </div>
-      <Image
-        src={urlFor(post.mainImage).url()}
-        alt={"Main Image"}
-        height={300}
-        width={600}
-        className={"rounded-lg w-full h-64 sm:h-[32rem] object-cover"}
-      />
-      <div className="max-w-none prose prose-invert sm:hover:prose-a:text-primary">
-        <PortableText value={post.body} components={PortableTextComponents} />
+    <div className="w-full max-w-4xl mx-auto px-2 sm:px-8">
+      <div className="flex flex-col space-y-12">
+        <div className="flex flex-col items-center space-y-2">
+          <p className="text-base text-secondary font-medium">{post.postDate}</p>
+          <h1 className="text-3xl sm:text-5xl font-bold text-center">{post.title}</h1>
+        </div>
+        <Image
+          src={urlFor(post.mainImage).url()}
+          alt={"Main Image"}
+          height={300}
+          width={600}
+          className={"rounded-lg w-full h-64 sm:h-[32rem] object-cover"}
+        />
+        <div className="prose prose-invert sm:hover:prose-a:text-primary max-w-none overflow-hidden">
+          <PortableText value={post.body} components={PortableTextComponents} />
+        </div>
       </div>
     </div>
   );
