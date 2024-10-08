f66cbbe54818230d29719081319ec7318cb8ebb9
Naufal Altaf <nao@altaf.xyz>
2024-10-08 23:39:22 +0700

    Adjust hero text on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
     <div className="w-full max-w-[49rem] px-4 flex flex-col justify-center gap-6">
-      <h1 className="text-center text-4xl md:text-7xl font-semibold">
+      <h1 className="text-center text-3xl md:text-7xl font-semibold">
         Student & Developer<br/>Based in Indonesia
       </h1>
-      <p className="text-center text-base md:text-lg text-gray-500">
+      <p className="text-center text-sm md:text-lg text-gray-500">
         Hi, I&apos;m Naufal, a student studying Information Systems. I have an interest in full stack and AOSP development.
       </p>
     </div>
