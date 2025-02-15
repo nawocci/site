117e57a586f249630dcbad4839e1045eb8d13591
Naufal Altaf <me@altaf.xyz>
2025-02-15 10:34:35 +0700

    Style the suspense page

--- src/app/(site)/blog/loading.tsx
+++ src/app/(site)/blog/loading.tsx
@@ -1,7 +1,10 @@
 export default function Loading() {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className="w-full flex items-center justify-center">
+      <div className="text-center">
+        <div className="w-12 h-12 border-t-2 border-primary rounded-full animate-spin mx-auto"></div>
+        <p className="mt-2 text-primary">Loading...</p>
+      </div>
     </div>
   );
 }
