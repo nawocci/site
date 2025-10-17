b80cda4945fe468f37a235c6429be88f43d56d5a
Naufal Altaf <me@altaf.xyz>
2025-10-18 00:14:33 +0700

    Create the home page hero section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="w-full h-full flex flex-col justify-center items-center gap-6">
+      <h1 className="text-center text-7xl font-semibold">
+        Student & Developer<br/>Based in Indonesia
+      </h1>
+      <p className="text-center text-xl brightness-50">
+        Hello, I&apos;m Naufal Altaf. I (sometimes) write, code, and build things that I like and use.<br/>I work on frontend, mobile development, and machine learning.
+      </p>
+    </div>
   );
 }
