ac68e8ed2914329a53e95f3cc1529e373006fb6d
Naufal Altaf <me@altaf.xyz>
2025-10-17 23:37:45 +0700

    Style the footer

--- src/app/(site)/components/Footer.tsx
+++ src/app/(site)/components/Footer.tsx
@@ -1,5 +1,51 @@
 export default function Footer() {
-    return (
-        <p>© {new Date().getFullYear()} Naufal Altaf. All rights reserved.</p>
-    )
+  return (
+    <footer className="flex items-center justify-center w-full min-h-32">
+      <div className="flex flex-col items-center gap-2 text-center text-sm">
+        <p>
+          &copy; {new Date().getFullYear()} Naufal Altaf. All rights reserved.
+        </p>
+        <div className="flex items-center gap-4">
+          <a 
+            href="https://github.com/nawocci" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:underline hover:text-primary hover:brightness-100"
+          >
+            GitHub
+          </a>
+          <a 
+            href="https://x.com/nawocci" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:underline hover:text-primary hover:brightness-100"
+          >
+            Twitter
+          </a>
+          <a 
+            href="https://instagram.com/nawocci" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:underline hover:text-primary hover:brightness-100"
+          >
+            Instagram
+          </a>
+          <a 
+            href="https://linkedin.com/in/naufal-altaf-059ab5326" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:underline hover:text-primary hover:brightness-100"
+          >
+            LinkedIn
+          </a>
+          <a 
+            href="mailto:me@altaf.xyz"
+            className="hover:underline hover:text-primary hover:brightness-100"
+          >
+            Email
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
