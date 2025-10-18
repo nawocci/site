954fc1d34ca2fe7bc79b5ca7636d0393f55e45bb
Naufal Altaf <me@altaf.xyz>
2025-10-18 21:07:22 +0700

    Reduce home page font size

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,10 +3,10 @@ import { HiOutlineDocumentText, HiOutlineLockClosed, HiOutlineShieldCheck } from
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-6 fade-in">
-      <h1 className="text-center text-4xl lg:text-7xl font-semibold">
+      <h1 className="text-center text-3xl lg:text-6xl font-semibold">
         Student & Developer<br/>Based in Indonesia
       </h1>
-      <p className="text-center text-base lg:text-xl brightness-[25%] dark:brightness-50">
+      <p className="text-center text-sm lg:text-lg brightness-[25%] dark:brightness-50">
         Hello, I&apos;m Naufal Altaf. I (sometimes) write, code, and build things that I like and use.<br className="hidden lg:block"/>
         <span className="lg:hidden"> </span>I work on frontend, mobile development, and machine learning.
       </p>
