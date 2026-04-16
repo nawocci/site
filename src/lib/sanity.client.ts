338ba1b4357720a506cafa59d338eeb3b4c36d7b
Naufal Altaf <me@altaf.xyz>
2026-04-16 19:09:37 +0700

    Adapt for Vercel

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,11 +1,13 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.SANITY_PROJECT_ID;
-const dataset = process.env.SANITY_DATASET;
+const projectId =
+  process.env.SANITY_PROJECT_ID ?? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset =
+  process.env.SANITY_DATASET ?? process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 if (!projectId || !dataset) {
   throw new Error(
-    "Missing SANITY_PROJECT_ID or SANITY_DATASET in environment variables."
+    "Missing Sanity env vars. Set SANITY_PROJECT_ID (or NEXT_PUBLIC_SANITY_PROJECT_ID) and SANITY_DATASET (or NEXT_PUBLIC_SANITY_DATASET)."
   );
 }
 
